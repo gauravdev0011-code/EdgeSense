334edd280fd5e0b03b5674ef03382584512ed25b
Gaurav Dev <gauravdev0011@gmail.com>
2026-08-11 00:44:19 -0400

    Add eight sensor streams

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,16 +1,23 @@
 #include "SensorSimulator.hpp"
 
 #include <iostream>
+#include <vector>
 
 int main() {
     using namespace edgesense;
 
-    SensorSimulator sensor(
-        "temperature-01",
-        SensorType::Temperature
-    );
+    std::vector<SensorSimulator> sensors = {
+        SensorSimulator("temperature-01", SensorType::Temperature),
+        SensorSimulator("temperature-02", SensorType::Temperature),
+        SensorSimulator("vibration-01", SensorType::Vibration),
+        SensorSimulator("vibration-02", SensorType::Vibration),
+        SensorSimulator("current-01", SensorType::Current),
+        SensorSimulator("current-02", SensorType::Current),
+        SensorSimulator("acceleration-01", SensorType::Acceleration),
+        SensorSimulator("acceleration-02", SensorType::Acceleration)
+    };
 
-    for (int i = 0; i < 5; ++i) {
+    for (auto& sensor : sensors) {
         SensorReading reading = sensor.read();
 
         std::cout << reading.sensorId
