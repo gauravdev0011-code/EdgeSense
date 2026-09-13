44677d19e82c66d65b5289b2e13cd8f8f3c9d0e9
gauravdev0011-code <gauravdev0011@gmail.com>
2026-09-12 22:34:45 -0500

    fix: isolate WebSocket++ compatibility layer

--- server/include/WebSocketServer.hpp
+++ server/include/WebSocketServer.hpp
@@ -1,15 +1,8 @@
 #pragma once
 
-#include <atomic>
 #include <cstdint>
 #include <memory>
-#include <mutex>
-#include <set>
 #include <string>
-#include <thread>
-
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
 
 namespace edgesense {
 
@@ -26,32 +19,8 @@ public:
     void broadcast(const std::string& message);
 
 private:
-    using Server =
-        websocketpp::server<websocketpp::config::asio>;
-
-    using ConnectionHandle =
-        websocketpp::connection_hdl;
-
-    std::uint16_t port;
-
-    std::atomic<bool> running{false};
-
-    std::thread serverThread;
-
-    std::unique_ptr<Server> server;
-
-    std::set<
-        ConnectionHandle,
-        std::owner_less<ConnectionHandle>
-    > connections;
-
-    std::mutex connectionsMutex;
-
-    void run();
-
-    void onOpen(ConnectionHandle handle);
-
-    void onClose(ConnectionHandle handle);
+    struct Impl;
+    std::unique_ptr<Impl> impl;
 };
 
 } // namespace edgesense
